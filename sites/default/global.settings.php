46ad86f1d7149172bd1cdd0e50611c8d3f71275e
Kjartan Kristjansson <kjartan.kristjansson@olavstoppen.no>
2015-01-13 09:34:15 +0100

    added global file and diff module

---
+++ sites/default/global.settings.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * String overrides:
+ *
+ * To override specific strings on your site with or without enabling the Locale
+ * module, add an entry to this list. This functionality allows you to change
+ * a small number of your site's default English language interface strings.
+ *
+ * Remove the leading hash signs to enable.
+ */
+# $conf['locale_custom_strings_en'][''] = array(
+#	'Navigation' => 'Site Menu',
+#	'Submitted by !username on !datetime' => 'Written by the amazing !username around !datetime',
+#	'Powered by <a href="@poweredby">Drupal</a>' => 'What rhymes with <a href="@poweredby">Drupal</a>?',
+#	'forum'      => 'Discussion board',
+#   '@count min' => '@count minutes',
+# );
+
+/**
